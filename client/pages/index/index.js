1ffe42f728c80808249de50e51621f98586df420
Chiaki <Chiaki530@outlook.com>
2018-06-08 14:17:00 +0800

    fix show-member display

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -3,134 +3,135 @@
 var util = require('../../utils/util.js');
 var app = getApp();
 Page({
-<<<<<<< HEAD
-  data: {
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    projects: [
-      {
-        proID: 0,
-        proName: 'Project 1',
-        proType: 'Course',
-        proInfo: 'This is the introduction of this project',
-        proStartDate: '2016-01-01',
-        proEndDate: '2017-01-01'
-        // proMembers: [
-        //   { name: 'Alice' },
-        //   { name: 'Bob' },
-        //   { name: 'Cindy' },
-        //   { name: 'David' }
-        // ]
-      },
-      {
-        proID: 1,
-        proName: 'Project 2',
-        proType: 'Intern',
-        proInfo: 'This is the introduction of this project',
-        proStartDate: '2016-03-01',
-        proEndDate: '2017-05-01'
-        // proMembers: [
-        //   { name: 'Tom' },
-        //   { name: 'Peter' },
-        //   { name: 'Tony' },
-        //   { name: 'Clement' }
-        // ]
-      },
-    ]
-  },
-  //事件处理函数
-  bindProTap: function() {
-    wx.navigateTo({
-      url: '../project/project'
-    });
-  },
-  
-  // onLoad: function () {
-  //   console.log('onLoad');
-  //   wx.getSetting({
-  //     success: function (res) {
-  //       if (res.authSetting['scope.userInfo']) {
-  //         // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-  //         wx.getUserInfo({
-  //           success: function (res) {
-  //             console.log(res.userInfo);
-  //             app.globalData.userInfo = res.userInfo;
-  //             console.log("user Info is saved");
-  //             //console.log(app.globalData.userInfo)
-  //           }
-  //         });
-  //       }
-  //     }
-  //   });
-  //   var that = this;
-  //   //调用应用实例的方法获取全局数据
-  //   this.getData();
-  // },
-
-  onLoad: function() {
-    console.info('loading index...');
-    // var that = this;
-    // getApp().checkLogin(function() {
-    //   that.load();
-    // });
-  },
-
-  load: function() {
-    var that = this;
-    getApp().request({
-      url: "/project",
-      success: function(res) {
-        wx.hideLoading();
-
-        if (res.statusCode !== 200) {
-          wx.showToast({
-            icon: 'none',
-            title: 'Wrong Request!'
-          });
-          return;
-        }
-
-        var projects = res.data.map(function(project) {
-          var format = {};
-          format.proID = project.project_id;
-          format.proName= project.name;
-          format.proType = project.type;
-          format.proStartDate = project.start_date;
-          format.proEndDate = project.end_date;
-          return format;
-        });
-
-        that.setData({ projects : projects });
-
-      }
-    });
-  },
-  upper: function () {
-    wx.showNavigationBarLoading();
-    this.refresh();
-    console.log("upper");
-    setTimeout(function(){wx.hideNavigationBarLoading();wx.stopPullDownRefresh();}, 2000);
-  },
-  lower: function (e) {
-    wx.showNavigationBarLoading();
-    var that = this;
-    setTimeout(function(){wx.hideNavigationBarLoading();that.nextLoad();}, 1000);
-    console.log("lower");
-  },
-  //scroll: function (e) {
-  //  console.log("scroll")
-  //},
-=======
     data: {
         userInfo: {},
         hasUserInfo: false,
         canIUse: wx.canIUse('button.open-type.getUserInfo'),
+        projects: [{
+                proID: 0,
+                proName: 'Project 1',
+                proType: 'Course',
+                proInfo: 'This is the introduction of this project',
+                proStartDate: '2016-01-01',
+                proEndDate: '2017-01-01'
+                    // proMembers: [
+                    //   { name: 'Alice' },
+                    //   { name: 'Bob' },
+                    //   { name: 'Cindy' },
+                    //   { name: 'David' }
+                    // ]
+            },
+            {
+                proID: 1,
+                proName: 'Project 2',
+                proType: 'Intern',
+                proInfo: 'This is the introduction of this project',
+                proStartDate: '2016-03-01',
+                proEndDate: '2017-05-01'
+                    // proMembers: [
+                    //   { name: 'Tom' },
+                    //   { name: 'Peter' },
+                    //   { name: 'Tony' },
+                    //   { name: 'Clement' }
+                    // ]
+            },
+        ],
         user: {
             projects: []
         },
         projects_length: 0
     },
+    //事件处理函数
+    bindProTap: function() {
+        wx.navigateTo({
+            url: '../project/project'
+        });
+    },
+
+    // onLoad: function () {
+    //   console.log('onLoad');
+    //   wx.getSetting({
+    //     success: function (res) {
+    //       if (res.authSetting['scope.userInfo']) {
+    //         // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+    //         wx.getUserInfo({
+    //           success: function (res) {
+    //             console.log(res.userInfo);
+    //             app.globalData.userInfo = res.userInfo;
+    //             console.log("user Info is saved");
+    //             //console.log(app.globalData.userInfo)
+    //           }
+    //         });
+    //       }
+    //     }
+    //   });
+    //   var that = this;
+    //   //调用应用实例的方法获取全局数据
+    //   this.getData();
+    // },
+
+    onLoad: function() {
+        console.info('loading index...');
+        // var that = this;
+        // getApp().checkLogin(function() {
+        //   that.load();
+        // });
+    },
+
+    load: function() {
+        var that = this;
+        getApp().request({
+            url: "/project",
+            success: function(res) {
+                wx.hideLoading();
+
+                if (res.statusCode !== 200) {
+                    wx.showToast({
+                        icon: 'none',
+                        title: 'Wrong Request!'
+                    });
+                    return;
+                }
+
+                var projects = res.data.map(function(project) {
+                    var format = {};
+                    format.proID = project.project_id;
+                    format.proName = project.name;
+                    format.proType = project.type;
+                    format.proStartDate = project.start_date;
+                    format.proEndDate = project.end_date;
+                    return format;
+                });
+
+                that.setData({ projects: projects });
+
+            },
+            fail: function(res) {
+                that.getData();
+            }
+        });
+    },
+    upper: function() {
+        wx.showNavigationBarLoading();
+        this.refresh();
+        console.log("upper");
+        setTimeout(function() {
+            wx.hideNavigationBarLoading();
+            wx.stopPullDownRefresh();
+        }, 2000);
+    },
+    lower: function(e) {
+        wx.showNavigationBarLoading();
+        var that = this;
+        setTimeout(function() {
+            wx.hideNavigationBarLoading();
+            that.nextLoad();
+        }, 1000);
+        console.log("lower");
+    },
+    //scroll: function (e) {
+    //  console.log("scroll")
+    //},
     //事件处理函数
     bindProTap: function() {
         wx.navigateTo({
@@ -191,7 +192,6 @@ Page({
                 console.log(data);
             });
     },
->>>>>>> 9fa56d742d966c5e78862eb140cf8eb8a848aa12
 
     //使用本地 fake 数据实现刷新效果
     getData: function() {
@@ -224,40 +224,7 @@ Page({
             })
         }, 3000)
 
-<<<<<<< HEAD
-  //使用本地 fake 数据实现刷新效果
-  getData: function(){
-    var feed = util.getData2();
-    console.log("loaddata");
-    var feed_data = feed.data;
-    this.setData({
-      feed:feed_data,
-      feed_length: feed_data.length
-    });
-  },
-  refresh: function(){
-    wx.showToast({
-      title: '刷新中',
-      icon: 'loading',
-      duration: 3000
-    });
-    var feed = util.getData2();
-    console.log("loaddata");
-    var feed_data = feed.data;
-    this.setData({
-      feed:feed_data,
-      feed_length: feed_data.length
-    });
-    setTimeout(function(){
-      wx.showToast({
-        title: '刷新成功',
-        icon: 'success',
-        duration: 2000
-      });
-    },3000);
-=======
     },
->>>>>>> 9fa56d742d966c5e78862eb140cf8eb8a848aa12
 
     //使用本地 fake 数据实现继续加载效果
     nextLoad: function() {
@@ -269,37 +236,6 @@ Page({
         var next = util.getProjectsFake();
         console.log("continueload");
 
-<<<<<<< HEAD
-  //使用本地 fake 数据实现继续加载效果
-  nextLoad: function(){
-    wx.showToast({
-      title: '加载中',
-      icon: 'loading',
-      duration: 4000
-    });
-    var next = util.getData2();
-    console.log("continueload");
-    var next_data = next.data;
-    this.setData({
-      feed: this.data.feed.concat(next_data),
-      feed_length: this.data.feed_length + _data.length
-    });
-    setTimeout(function(){
-      wx.showToast({
-        title: '加载成功',
-        icon: 'success',
-        duration: 2000
-      });
-    },3000);
-  },
-  createProject: function () {
-    wx.navigateTo({
-      url: '../newProject/newProject'
-    });
-  },
-
-});
-=======
         this.setData({
             'user.projects': this.data.user.projects.concat(next),
             projects_length: this.data.projects_length + next.length
@@ -319,4 +255,3 @@ Page({
     },
 
 })
->>>>>>> 9fa56d742d966c5e78862eb140cf8eb8a848aa12
--- client/pages/project/project.js
+++ client/pages/project/project.js
@@ -1,14 +1,6 @@
-/*
- * @Date: 2018-06-07 21:51:16 
- * @Last Modified time: 2018-06-07 21:51:16 
- * @Problems: 1. what is e.currentTarget.id; 2. how to fetch the images from the server; 
- * 3. what does files store
- */
-
 var sliderWidth = 108; // 需要设置slider的宽度，用于计算中间位置
-const util = require('../../utils/util.js');
-const app = getApp(); //获得小程序实例
-const config = require('../../config.js');
+const util = require('../../utils/util.js')
+const app = getApp() //获得小程序实例
 
 Page({
     data: {
@@ -21,131 +13,117 @@ Page({
         project: {},
         tasks_length: 0,
 
-  },
-  onShareAppMessage: function (res) {
-    if (res.from === 'button') {
-      // 来自页面内转发按钮
-      console.log(res.target);
-    }
-    return {
-      title: 'Invite some people',
-      path: '/page/project/project?id=' + this.data.projectID
-    };
-  },
+        logs: [],
+        leftCount: 0,
 
-  save: function () {
-    var key1 = this.data.projectID + '-tasks';
-    var key2 = this.data.projectID + '-logs';
-    wx.setStorageSync(key1, this.data.tasks);
-    wx.setStorageSync(key2, this.data.logs);
-  },
-
-  load: function () {
-    //不建议使用本地存储，因为信息总是在变化
-    var key1 = this.data.projectID + '-tasks';
-    var key2 = this.data.projectID + '-logs';
-    var tasks = wx.getStorageSync(key1);
-    if (tasks) {
-      var leftCount = tasks.filter(function (item) {
-        return !item.completed;
-      }).length;
-      this.setData({ tasks: tasks, leftCount: leftCount });
-    }
-    else {
-      app.request({
-        url: "/project/"+this.data.projectID,
-        success: function(res) {
-          wx.hideLoading();
-  
-          if (res.statusCode !== 200) {
-            wx.showToast({
-              icon: 'none',
-              title: 'Wrong Request!'
-            });
-            return;
-          }
-          var project = res.data[0];
-          var members = res.data[1];
-          // formalize the task, no member information, start and end date both needed
-          var tasks = res.data[2].map(function(task) {
-            var format_task = {};
-            format_task.taskID = task.task_id;
-            format_task.taskName= task.name;
-            format_task.taskType = task.type;
-            format_task.taskStartDate = task.start_date;
-            format_task.taskEndDate = task.end_date;
-            return format_task;
-          });
-          
-          this.setData({
-              projectID: project.project_id,
-              proName: project.name,
-              proDes: project.info,
-              proMembers: members,
-              tasks: tasks
-          });
+    },
+    onShareAppMessage: function(res) {
+        if (res.from === 'button') {
+            // 来自页面内转发按钮
+            console.log(res.target)
         }
-      });
-    }
-    var logs = wx.getStorageSync(key2);
-    if (logs) {
-      this.setData({ logs: logs });
-    }
-    else {
-      app.request({
-        url: "/project/logs/"+this.data.projectID,
-        success: function(res) {
-          if (res.statusCode !== 200) {
-            wx.showToast({
-              icon: 'none',
-              title: 'Wrong Request!'
-            });
-            return;
-          }
-          
-          this.setData({
-            logs: res.data
-          });
+        return {
+            title: 'Invite some people',
+            path: '/page/project/project?id=' + this.data.project.projectID
         }
-      });
-    }
-  },
+    },
 
-  onLoad: function (opt) {
-    // var that = this;
-    wx.getSystemInfo({
-      success: function (res) {
-        that.setData({
-          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
-          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+    save: function() {
+        var key1 = this.data.project.projectID + '-tasks'
+        var key2 = this.data.project.projectID + '-logs'
+        wx.setStorageSync(key1, this.data.project.tasks)
+        wx.setStorageSync(key2, this.data.project.logs)
+    },
+
+    load: function() {
+        var key1 = this.data.project.projectID + '-tasks'
+        var key2 = this.data.project.projectID + '-logs'
+        var tasks = wx.getStorageSync(key1)
+        if (this.data.project.tasks) {
+            var leftCount = project.tasks.filter(function(item) {
+                return !item.completed
+            }).length
+            this.setData({ 'project.tasks': tasks, leftCount: leftCount })
+        }
+        var logs = wx.getStorageSync(key2)
+        if (logs) {
+            this.setData({ 'project.logs': logs })
+        }
+    },
+
+    onLoad: function(opt) {
+        var that = this;
+        wx.getSystemInfo({
+            success: function(res) {
+                that.setData({
+                    sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+                    sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+                });
+            }
         });
-      }
-    });
-    this.load();
-    //get the project id from the router
-    this.data.projectID = opt.id;
-    //get project info by local storage or wx request 
+        this.load()
+            //get the project id from the router
+        this.data.project.projectID = opt.id
+            //get project info by local storage or wx request 
+        this.getData();
+        //check if it is member 
+        this.addMember();
 
-    var isMember = false;
-    for (var item in this.data.proMembers) {
-      if (item.name == app.globalData.userInfo.nickName)
-        isMember = true;
-    }
-    if (!isMember) {
-      var members = this.data.proMembers;
-      members.push({ name: app.globalData.userInfo.nickName, avatarUrl: app.globalData.userInfo.avatarUrl});
-      this.setData({
-        proMembers: members
-      });
-      console.log('Add Member ' + app.globalData.userInfo.nickName);
-      var logs = this.data.logs;
-      logs.push({ timestamp: util.formatTime(new Date()), action: 'Become New Member', actionInfo: '', userInfo: app.globalData.userInfo });
-      this.setData({
-        logs: logs
-      });
-      this.save();
-    }
-  },
+        var that = this
+        wx.getSystemInfo({
+            success: function(res) {
+                that.setData({
+                    sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+                    sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+                })
+            }
+        })
+    },
+
+    //check member 
+    addMember: function() {
+        var isMember = false
+        for (var item in this.data.project.proMembers) {
+            if (item.name == app.globalData.userInfo.nickName)
+                isMember = true
+        }
+        if (!isMember) {
+            var members = this.data.project.proMembers
+            members.push({ name: app.globalData.userInfo.nickName, avatarUrl: app.globalData.userInfo.avatarUrl })
+            this.setData({
+                'project.proMembers': members
+            })
+            console.log('Add Member ' + app.globalData.userInfo.nickName)
+            var logs = this.data.project.logs
+            logs.push({ timestamp: util.formatTime(new Date()), action: 'Become New Member', actionInfo: '', userInfo: app.globalData.userInfo })
+            this.setData({
+                'project.logs': logs
+            })
+            this.save()
+        }
+    },
+    //get Data and refresh
+    upper: function() {
+        wx.showNavigationBarLoading()
+        this.refresh();
+        console.log("upper");
+        setTimeout(function() {
+            wx.hideNavigationBarLoading();
+            wx.stopPullDownRefresh();
+        }, 2000);
+    },
+    lower: function(e) {
+        wx.showNavigationBarLoading();
+        var that = this;
+        setTimeout(function() {
+            wx.hideNavigationBarLoading();
+            that.nextLoad();
+        }, 1000);
+        console.log("lower")
+    },
+    //scroll: function (e) {
+    //  console.log("scroll")
+    //},
 
     //网络请求数据, 实现首页刷新
     refresh0: function() {
@@ -163,7 +141,7 @@ Page({
     getData: function() {
         var feed = util.getAProjectFake();
         console.log("get a project");
-        
+        console.log(feed);
         this.setData({
             project: feed,
             tasks_length: feed.tasks.length
@@ -177,7 +155,7 @@ Page({
         });
         var feed = util.getAProjectFake();
         console.log("refresh to get a project");
-        // var feed = feed;
+        var feed = feed;
         this.setData({
             project: feed,
             tasks_length: feed.tasks.length
@@ -187,8 +165,8 @@ Page({
                 title: '刷新成功',
                 icon: 'success',
                 duration: 2000
-            });
-        }, 3000);
+            })
+        }, 3000)
 
     },
 
@@ -198,7 +176,7 @@ Page({
             title: '加载中',
             icon: 'loading',
             duration: 4000
-        });
+        })
         var next = util.getAProjectFake();
         console.log("continueload");
 
@@ -211,18 +189,18 @@ Page({
                 title: '加载成功',
                 icon: 'success',
                 duration: 2000
-            });
-        }, 3000);
+            })
+        }, 3000)
     },
 
 
 
     //onShow function for Logs 
     onShow: function() {
-        var key2 = this.data.project.projectID + '-logs';
-        var logs = wx.getStorageSync(key2);
+        var key2 = this.data.project.projectID + '-logs'
+        var logs = wx.getStorageSync(key2)
         if (logs) {
-            this.setData({ 'project.logs': logs.reverse() });
+            this.setData({ 'project.logs': logs.reverse() })
         }
     },
 
@@ -231,103 +209,103 @@ Page({
             sliderOffset: e.currentTarget.offsetLeft,
             activeIndex: e.currentTarget.id
         });
-        //上传图片至服务器
-        wx.uploadFile({
-          header: {
-            skey: wx.getStorageSync('skey')
-          },
-          url: config.host + '/user/image/'+this.data.projectID,
-          filePath: res.tempFilePaths[0],
-          name: 'image'
-          // success: function(res) {
-          //   getApp().request({
-          //     url: '/user',
-          //     method: 'patch',
-          //     data: {
-          //       avatar: res.data
-          //     },
-          //     success: function () {
-          //       wx.hideLoading();
-          //       getApp().globalData.userInfo.avatar = res.data;
-          //       that.setData({ avatar: res.data });
-          //     }
-          //   });
+    },
 
-          // }
-        });
+    //Navigate to newTask Page
+    createTask: function() {
+        wx.navigateTo({
+            url: '../newTask/newTask?id=' + this.data.project.projectID
+        })
+    },
 
-      },
+    chooseImage: function(e) {
+        var that = this;
+        wx.chooseImage({
+            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+            success: function(res) {
+                // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+                that.setData({
+                    files: that.data.files.concat(res.tempFilePaths)
+                });
+                //上传图片至服务器
+                var tempFilePaths = res.tempFilePaths
+                    /* 上传图片接口
+                    wx.uploadFile({
+                      url: 'https://example.weixin.qq.com/upload', //仅为示例，非真实的接口地址
+                      filePath: tempFilePaths[0],
+                      name: 'file',
+                      formData: {
+                        'user': 'test'
+                      },
+                      success: function (res) {
+                        var data = res.data
+                        //do something
+                      }
+                    }) */
 
-  previewImage: function (e) {
-    if (!this.data.showDeleteIcon) {
-      var that = this;
-      var file_id = e.currentTarget.id;
-      var file_index = file_id.lastIndexOf('.');
-      file_id = file_id.substring(file_index + 1);
-      console.log(file_id);
-      console.log(e.currentTarget);
-      wx.getImageInfo({
-        src: e.currentTarget.id,
-        success: function (res) {
-          //The file is an Image 
-          console.log(res.type);
-          wx.previewImage({
-            current: e.currentTarget.id, // 当前显示图片的http链接
-            urls: that.data.files // 需要预览的图片http链接列表
-          });
-        },
-        fail: function () {
-          wx.openDocument({
-            filePath: e.currentTarget.id,
-            success: function (res) {
-              console.log("打开文件成功");
-            },
-            fail: function () {
-              console.log("不支持打开该文件");
             }
-          });
+        })
+    },
+    previewImage: function(e) {
+        if (!this.data.showDeleteIcon) {
+            var that = this
+            var file_id = e.currentTarget.id
+            var file_index = file_id.lastIndexOf('.')
+            file_id = file_id.substring(file_index + 1)
+            console.log(file_id)
+            console.log(e.currentTarget)
+            wx.getImageInfo({
+                src: e.currentTarget.id,
+                success: function(res) {
+                    //The file is an Image 
+                    console.log(res.type)
+                    wx.previewImage({
+                        current: e.currentTarget.id, // 当前显示图片的http链接
+                        urls: that.data.files // 需要预览的图片http链接列表
+                    })
+                },
+                fail: function() {
+                    wx.openDocument({
+                        filePath: e.currentTarget.id,
+                        success: function(res) {
+                            console.log("打开文件成功")
+                        },
+                        fail: function() {
+                            console.log("不支持打开该文件")
+                        }
+                    })
+                }
+            })
+        } else {
+            this.deleteFile(e)
         }
-      });
-    }
-    else {
-      this.deleteFile(e);
-    }
-  },
-  // onShareAppMessage: function () {
-  //   return {
-  //     path: 'pages/project/project?id=' + this.data.projectID,
-  //     success: res => {
-  //       console.log(res)
-  //     }
-  //   }
-  // },
+    },
+    onShareAppMessage: function() {
+        return {
+            path: 'pages/project/project?id=' + this.data.project.projectID,
+            success: res => {
+                console.log(res)
+            }
+        }
+    },
 
-  // what is e.currentTarget.id?
-  deleteFile: function (e) {
-    console.log(e.currentTarget.id);
-    this.data.files.splice(e.currentTarget.id, 1);
-    this.setData({
-      files: this.data.files
-    });
-    app.request({
-      url: config.host+'/project/'+this.data.projectID,
-      method: 'delete',
-      success: function(res){
-        var files = this.data.files;
-        files.splice(res.data.url, 1);
-        this.setData({files: files});
-      }
-    });
-  },
+    deleteFile: function(e) {
+        console.log(e.currentTarget.id)
+        this.data.files.splice(e.currentTarget.id, 1)
+        this.setData({
+            files: this.data.files
+        })
+    },
 
-  showDelete: function (e) {
-    this.setData({
-      showDeleteIcon: true
-    });
-  },
-  hideDelete: function () {
-    this.setData({
-      showDeleteIcon: false
-    });
-  }
+    showDelete: function(e) {
+        this.setData({
+            showDeleteIcon: true
+        })
+    },
+    hideDelete: function() {
+        this.setData({
+            showDeleteIcon: false
+        })
+    }
 });
